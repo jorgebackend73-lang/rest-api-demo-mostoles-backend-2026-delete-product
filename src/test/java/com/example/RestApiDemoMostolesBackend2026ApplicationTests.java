c23767abf95d5e991db45209ab0089868b8b8696
jorgebackend73-lang <jorgebackend73@gmail.com>
2026-09-21 10:18:57 +0200

    test a la capa dao o de repositorios

--- src/test/java/com/example/RestApiDemoMostolesBackend2026ApplicationTests.java
+++ src/test/java/com/example/RestApiDemoMostolesBackend2026ApplicationTests.java
@@ -6,6 +6,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class RestApiDemoMostolesBackend2026ApplicationTests {
 
+	// Esta clase nos levanta todo el contexto de spring, se crea sola en la parte test del proyecto.
+	// Con [ mvn test ] en la terminal abierta desde el directorio del proyecto lanzamos todos los test.
+	// Para ejecutar un test en concreto [ mvn test -Dtest=NombreClase#nombreDelTest ]
+	// En nuestro caso [ mvn test -Dtest=ProductDaoTest#testSaveProduct <- metodo implementado ]
 	@Test
 	void contextLoads() {
 	}
